676407dbbdd499c70b72c43d881ef253ae683699
Sherzod <sher2142658@gmail.com>
2022-03-16 00:12:14 +0300

    calculator-tip

---
+++ script.js
@@ -0,0 +1,44 @@
+let billInput = document.querySelector('.form__input_bill');
+let qualitySelect = document.querySelector('.form__input_quality');
+let peopleInput = document.querySelector('.form__input_people');
+
+let calcButton = document.querySelector('.form__button_calc');
+let resetButton = document.querySelector('.form__button_reset');
+
+calcButton.addEventListener('click', calcClickHAndler);
+resetButton.addEventListener('click', resetClickHandler);
+
+function calcClickHAndler() {
+    let bill = parseInt(billInput.value);
+    let quality = parseInt(qualitySelect.value);
+    let people = parseInt(peopleInput.value);
+
+    let tips = calculateTips(bill, quality);
+    let tipsForEach = tips / people;
+    let totalForEach = calculateTotal(bill, tips) / people
+
+    renderResults(tipsForEach, totalForEach);
+
+}
+
+function resetClickHandler() {
+    billInput.value = '';
+    qualitySelect.value = '';
+    peopleInput.value = '';
+
+    renderResults('___', '___');
+}
+
+function calculateTips(summ, percent) {
+    return summ * percent / 100;
+}
+
+function calculateTotal(summ, tips) {
+    return summ + tips;
+}
+
+function renderResults(tips, total) {
+    document.querySelector('.results__value_tips').innerText = tips + '₽';
+    document.querySelector('.results__value_total').innerText = total + '₽';
+}
+
